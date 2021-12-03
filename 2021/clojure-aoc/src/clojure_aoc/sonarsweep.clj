1d6d598b4a9bbfe4da0794c973a584ceab2e0d16
Gahan Rakholia <gahan94rakh@gmail.com>
2021-12-03 13:50:11 +0530

    acc-day1: sonar-sweep part 2 solution

--- 2021/clojure-aoc/src/clojure_aoc/sonarsweep.clj
+++ 2021/clojure-aoc/src/clojure_aoc/sonarsweep.clj
@@ -7,3 +7,13 @@
        (partition 2 1)
        (filter #(< (first %) (second %)))
        count))
+
+(defn calc-increment-window [& depths]
+  (->> depths
+       (flatten)
+       (map #(Integer/parseInt %))
+       (partition 3 1)
+       (reduce #(conj %1 (reduce + %2)) [])
+       (partition 2 1)
+       (filter #(< (first %) (second %)))
+       count))
--- 2021/clojure-aoc/test/clojure_aoc/sonarsweep_test.clj
+++ 2021/clojure-aoc/test/clojure_aoc/sonarsweep_test.clj
@@ -14,11 +14,25 @@
   (testing "with monotonically increase"
     (is (= 2 (calc-increment "3" "4" "4" "2" "2" "5"))))
   (testing "with all the values same"
-    (is (= 0 (calc-increment "2" "2" "2" "2")))))
-
-(deftest calc-increment-my-input
+    (is (= 0 (calc-increment "2" "2" "2" "2"))))
   (testing "read from testdata file and assert"
     (with-open [rdr (io/reader "test/clojure_aoc/sonarsweep-testdata.txt")]
       (is (= 1462 (calc-increment (line-seq rdr)))))))
 
-
+(deftest calc-increment-window-test
+  (testing "with sample input"
+    (is (= 0 (calc-increment-window "1" "2" "3")))
+    (is (= 5 (calc-increment-window "199" "200" "208" "210" "200" "207" "240" "269" "260" "263"))))
+  (testing "when input is not multiple of window size"
+    (is (= 2 (calc-increment-window "1" "2" "3" "4" "5"))))
+  (testing "with empty input"
+    (is (= 0 (calc-increment-window))))
+  (testing "with input smaller than window size"
+    (is (= 0 (calc-increment-window "1" "2"))))
+  (testing "when window sum is same"
+    (is (= 0 (calc-increment-window "1" "2" "3" "1"))))
+  (testing "when window sum is decreasing"
+    (is (= 0 (calc-increment-window "4" "3" "2" "1"))))
+  (testing "read from testdata file and assert"
+    (with-open [rdr (io/reader "test/clojure_aoc/sonarsweep-testdata.txt")]
+      (is (= 1497 (calc-increment-window (line-seq rdr)))))))
