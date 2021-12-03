01c2f926dac10aa371b51472c0203af9ada4d6da
Gahan Rakholia <gahan94rakh@gmail.com>
2021-12-03 14:07:02 +0530

    refactor: extract input parsing to separate function

--- 2021/clojure-aoc/src/clojure_aoc/sonarsweep.clj
+++ 2021/clojure-aoc/src/clojure_aoc/sonarsweep.clj
@@ -1,19 +1,19 @@
 (ns clojure-aoc.sonarsweep)
 
-(defn calc-increment [& depths]
+(defn calc-increment [depths]
   (->> depths
-       (flatten)
-       (map #(Integer/parseInt %))
        (partition 2 1)
        (filter #(< (first %) (second %)))
        count))
 
-(defn calc-increment-window [& depths]
+(defn calc-increment-window [depths]
   (->> depths
-       (flatten)
-       (map #(Integer/parseInt %))
        (partition 3 1)
        (reduce #(conj %1 (reduce + %2)) [])
-       (partition 2 1)
-       (filter #(< (first %) (second %)))
-       count))
+       calc-increment))
+
+(defn parse-input [& depths]
+  (->> depths
+       (flatten)
+       (filter #(not= "" %))
+       (map #(Integer/parseInt %))))
--- 2021/clojure-aoc/test/clojure_aoc/sonarsweep_test.clj
+++ 2021/clojure-aoc/test/clojure_aoc/sonarsweep_test.clj
@@ -5,34 +5,34 @@
 
 (deftest calc-increment-test
   (testing "with sample input"
-    (is (= 2 (calc-increment "1" "2" "3")))
-    (is (= 7 (calc-increment "199" "200" "208" "210" "200" "207" "240" "269" "260" "263"))))
+    (is (= 2 (calc-increment (parse-input "1" "2" "3"))))
+    (is (= 7 (calc-increment (parse-input "199" "200" "208" "210" "200" "207" "240" "269" "260" "263")))))
   (testing "with empty input"
-    (is (= 0 (calc-increment))))
+    (is (= 0 (calc-increment (parse-input "")))))
   (testing "with strictly decreasing"
-    (is (= 0 (calc-increment "3" "2" "1"))))
+    (is (= 0 (calc-increment (parse-input "3" "2" "1")))))
   (testing "with monotonically increase"
-    (is (= 2 (calc-increment "3" "4" "4" "2" "2" "5"))))
+    (is (= 2 (calc-increment (parse-input "3" "4" "4" "2" "2" "5")))))
   (testing "with all the values same"
-    (is (= 0 (calc-increment "2" "2" "2" "2"))))
+    (is (= 0 (calc-increment (parse-input "2" "2" "2" "2")))))
   (testing "read from testdata file and assert"
     (with-open [rdr (io/reader "test/clojure_aoc/sonarsweep-testdata.txt")]
-      (is (= 1462 (calc-increment (line-seq rdr)))))))
+      (is (= 1462 (calc-increment (parse-input (line-seq rdr))))))))
 
 (deftest calc-increment-window-test
   (testing "with sample input"
-    (is (= 0 (calc-increment-window "1" "2" "3")))
-    (is (= 5 (calc-increment-window "199" "200" "208" "210" "200" "207" "240" "269" "260" "263"))))
+    (is (= 0 (calc-increment-window (parse-input "1" "2" "3"))))
+    (is (= 5 (calc-increment-window (parse-input "199" "200" "208" "210" "200" "207" "240" "269" "260" "263")))))
   (testing "when input is not multiple of window size"
-    (is (= 2 (calc-increment-window "1" "2" "3" "4" "5"))))
+    (is (= 2 (calc-increment-window (parse-input "1" "2" "3" "4" "5")))))
   (testing "with empty input"
-    (is (= 0 (calc-increment-window))))
+    (is (= 0 (calc-increment-window (parse-input "")))))
   (testing "with input smaller than window size"
-    (is (= 0 (calc-increment-window "1" "2"))))
+    (is (= 0 (calc-increment-window (parse-input "1" "2")))))
   (testing "when window sum is same"
-    (is (= 0 (calc-increment-window "1" "2" "3" "1"))))
+    (is (= 0 (calc-increment-window (parse-input "1" "2" "3" "1")))))
   (testing "when window sum is decreasing"
-    (is (= 0 (calc-increment-window "4" "3" "2" "1"))))
+    (is (= 0 (calc-increment-window (parse-input "4" "3" "2" "1")))))
   (testing "read from testdata file and assert"
     (with-open [rdr (io/reader "test/clojure_aoc/sonarsweep-testdata.txt")]
-      (is (= 1497 (calc-increment-window (line-seq rdr)))))))
+      (is (= 1497 (calc-increment-window (parse-input (line-seq rdr))))))))
