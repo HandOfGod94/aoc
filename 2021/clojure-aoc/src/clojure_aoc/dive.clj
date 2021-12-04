e2f3367f859915545a1760432fa8494996879bc9
Gahan Rakholia <gahan94rakh@gmail.com>
2021-12-05 03:53:35 +0530

    aoc-day2[part 2]: solve for dive problem

--- 2021/clojure-aoc/src/clojure_aoc/dive.clj
+++ 2021/clojure-aoc/src/clojure_aoc/dive.clj
@@ -1,14 +1,27 @@
 (ns clojure-aoc.dive
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.core.match :refer [match]]))
+
+(defn- coord-product [final-coord]
+  (* (first final-coord) (second final-coord)))
 
 (defn final-coord [inputs]
-  ())
+  (->> inputs
+       (reduce (fn [position command]
+                 (match command
+                   [:forward x] [(+ (first position) x) (second position)]
+                   [:down y] [(first position) (+ (second position) y)]
+                   [:up y] [(first position) (- (second position) y)]
+                   :else position))
+               [0 0])
+       (coord-product)))
 
 (defn parse-line [line]
   (as-> line v
     (str/split v #" ")
-    (list (keyword (first v)) (Integer/parseInt (second v)))))
+    (vector (keyword (first v)) (Integer/parseInt (second v)))))
 
 (defn parse-input [& args]
   (->> args
+       (flatten)
        (map parse-line)))
--- 2021/clojure-aoc/test/clojure_aoc/dive_test.clj
+++ 2021/clojure-aoc/test/clojure_aoc/dive_test.clj
@@ -1,11 +1,25 @@
 (ns clojure-aoc.dive-test
   (:require
    [clojure.test :refer [deftest is testing]]
-   [clojure-aoc.dive :as dive]))
+   [clojure-aoc.dive :as dive]
+   [clojure.java.io :as io]))
 
 (deftest parse-line-test
   (testing "line in correct format"
-    (is (= `(:forward 5) (dive/parse-line "forward 5")))))
+    (is (= [:forward 5] (dive/parse-line "forward 5")))))
 
 (deftest parse-input-test
-  (is (= `((:forward 5) (:down 3)) (dive/parse-input "forward 5" "down 3"))))
+  (is (= [[:forward 5] [:down 3]] (dive/parse-input "forward 5" "down 3"))))
+
+
+(deftest dive-test
+  (testing "with sample input"
+    (is (= 150 (dive/final-coord [[:forward 5]
+                                  [:down 5]
+                                  [:forward 8]
+                                  [:up 3]
+                                  [:down 8]
+                                  [:forward 2]]))))
+  (testing "with my input"
+    (with-open [rdr (io/reader "test/clojure_aoc/dive-testdata.txt")]
+      (is (= 1746616 (dive/final-coord (dive/parse-input (line-seq rdr))))))))
